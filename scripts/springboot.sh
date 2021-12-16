110f61121105f63a8afab7b23cdfdcecdddf480f
Kim-Yang-Jae <kyj-9488@hanmail.net>
2021-12-16 20:12:47 +0900

    211216| shell script 수정

--- scripts/springboot.sh
+++ scripts/springboot.sh
@@ -10,13 +10,13 @@ CURRENT_DEPLOY_DIR_PATH=${DEPLOY_DIR_PATH}/${PRJT_NAME}/current
 start() {
   echo ">> server start"
   JAR_FILE=$(basename ${CURRENT_DEPLOY_DIR_PATH}/*.jar)
-  nohup java -jar "${CURRENT_DEPLOY_DIR_PATH}/${JAR_FILE}" \
+  sudo nohup java -jar "${CURRENT_DEPLOY_DIR_PATH}/${JAR_FILE}" \
         -Dspring.profiles.active=$ENV -Dsever.port=$PORT_NUM &
 }
 
 stop() {
   STOP_PROCESS=$(ps -ef | grep $ENV | grep $PORT_NUM | grep $PRJT_NAME | awk '{print $2}' )
-  kill -9 "$STOP_PROCESS"
+  sudo kill -9 "$STOP_PROCESS"
 }
 
 if [ "$ACTION" == "start" ]; then
