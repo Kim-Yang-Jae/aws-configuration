05f0c3af6175b4025c502c1c72ee54d1eba5d32b
Kim-Yang-Jae <kyj-9488@hanmail.net>
2021-12-21 22:28:11 +0900

    211221| shell script 수정(path 확인)

--- scripts/build-postprocess.sh
+++ scripts/build-postprocess.sh
@@ -22,10 +22,14 @@ if [ ! -d ${DEPLOY_DIR_PATH}/${PROJECT_NAME}/${CHANGED_JAR_FILE_NAME} ]; then
 fi
 
 echo ">> move jar file"
-sudo mv ./build/libs/*.jar ${DEPLOY_DIR_PATH}/${PROJECT_NAME}/"${CHANGED_JAR_FILE_NAME}"/"${CHANGED_JAR_FILE_NAME}".jar
+sudo mv ./build/libs/*.jar "${DEPLOY_DIR_PATH}/${PROJECT_NAME}/${CHANGED_JAR_FILE_NAME}/${CHANGED_JAR_FILE_NAME}.jar"
 
 
 echo ">>make symbolic link"
 echo "target-path : ${DEPLOY_DIR_PATH}/${PROJECT_NAME}/${CHANGED_JAR_FILE_NAME}"
 echo "link-path : ${CURRENT_DEPLOY_DIR_PATH}"
+if [ -d "${CURRENT_DEPLOY_DIR_PATH}" ]; then
+  echo ">>symbolic link is already exist"
+  sudo rm -f ${CURRENT_DEPLOY_DIR_PATH}
+fi
 sudo ln -s ${DEPLOY_DIR_PATH}/"${PROJECT_NAME}"/"${CHANGED_JAR_FILE_NAME}" "${CURRENT_DEPLOY_DIR_PATH}"
