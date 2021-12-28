3ce9a32130b689ecdf38ca499e702d4f59ebe8da
Kim-Yang-Jae <kyj-9488@hanmail.net>
2021-12-28 20:53:17 +0900

    211228| 불필요한 sudo 명령 제거

--- scripts/build-postprocess.sh
+++ scripts/build-postprocess.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 PROJECT_NAME=$1
-PORT_NUM=$2
 
 DEPLOY_DIR_PATH=/home/ubuntu/deploy
-LOG_DIR_PATH=/home/ubuntu/logs
 CURRENT_DEPLOY_DIR_PATH=${DEPLOY_DIR_PATH}/${PROJECT_NAME}/current
 
 TODAY=$(date "+%Y%m%d%H%M%S")
@@ -24,13 +22,13 @@ if [ ! -d ${DEPLOY_DIR_PATH}/${PROJECT_NAME}/${CHANGED_JAR_FILE_NAME} ]; then
 fi
 
 echo ">> move jar file"
-sudo mv ./build/libs/*.jar "${DEPLOY_DIR_PATH}/${PROJECT_NAME}/${CHANGED_JAR_FILE_NAME}/${CHANGED_JAR_FILE_NAME}.jar"
+mv ./build/libs/*.jar "${DEPLOY_DIR_PATH}/${PROJECT_NAME}/${CHANGED_JAR_FILE_NAME}/${CHANGED_JAR_FILE_NAME}.jar"
 
 echo ">>make symbolic link"
 echo "target-path : ${DEPLOY_DIR_PATH}/${PROJECT_NAME}/${CHANGED_JAR_FILE_NAME}"
 echo "link-path : ${CURRENT_DEPLOY_DIR_PATH}"
 if [ -d "${CURRENT_DEPLOY_DIR_PATH}" ]; then
   echo ">>symbolic link is already exist"
-  sudo rm -f ${CURRENT_DEPLOY_DIR_PATH}
+  rm -f ${CURRENT_DEPLOY_DIR_PATH}
 fi
-sudo ln -s ${DEPLOY_DIR_PATH}/"${PROJECT_NAME}"/"${CHANGED_JAR_FILE_NAME}" "${CURRENT_DEPLOY_DIR_PATH}"
+ln -s ${DEPLOY_DIR_PATH}/"${PROJECT_NAME}"/"${CHANGED_JAR_FILE_NAME}" "${CURRENT_DEPLOY_DIR_PATH}"
--- scripts/springboot.sh
+++ scripts/springboot.sh
@@ -8,18 +8,17 @@ DEPLOY_DIR_PATH=/home/ubuntu/deploy
 CURRENT_DEPLOY_DIR_PATH=${DEPLOY_DIR_PATH}/${PRJT_NAME}/current
 
 start() {
-  echo $(whoami)
   echo ">> start method"
-  JAR_FILE=$(basename ${CURRENT_DEPLOY_DIR_PATH}/*.jar)
+  JAR_FILE=$(basename "${CURRENT_DEPLOY_DIR_PATH}"/*.jar)
   echo ">> jar-file-name : $JAR_FILE"
-  sudo chmod 755 "${CURRENT_DEPLOY_DIR_PATH}/${JAR_FILE}"
-  nohup java -jar -Dspring.profiles.active=$ENV -Dsever.port=$PORT_NUM \
+#  sudo chmod 755 "${CURRENT_DEPLOY_DIR_PATH}/${JAR_FILE}"
+  nohup java -jar -Dspring.profiles.active="$ENV" -Dsever.port="$PORT_NUM" \
           "${CURRENT_DEPLOY_DIR_PATH}/${JAR_FILE}" &
 }
 
 stop() {
   echo ">> stop method"
-  STOP_PROCESS=$(ps -ef | grep $ENV | grep $PORT_NUM | grep $PRJT_NAME | awk '{print $2}' )
+  STOP_PROCESS=$(ps -ef | grep "$ENV" | grep "$PORT_NUM" | grep "$PRJT_NAME" | awk '{print $2}' )
   sudo kill -9 "$STOP_PROCESS"
 }
 
